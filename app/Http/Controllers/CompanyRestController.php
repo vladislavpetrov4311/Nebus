ce53c8741eca1b0a2565bba7f4adf5ac6e604e84
Vladislav4311 <petrov_vd_4311@mail.ru>
2025-02-08 21:23:50 +0300

    Шаг 8. расшифровываем JWT токен

--- app/Http/Controllers/CompanyRestController.php
+++ app/Http/Controllers/CompanyRestController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CompanyRequest;
+use Firebase\JWT\JWT;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\Key;
 
 class CompanyRestController extends Controller {
 
     public function __invoke(CompanyRequest $request) {
-        dd($request->header('key_token'));
+
+        $token = $request->bearerToken();
+    
+        $key = 'Nebus'; // ! в Postman не нужно шифровать ключ !, потому что он шифрует некорректно, поэтому проверям открыто
+
+        dd($decoded = JWT::decode($token, new Key($key, 'HS256'))); 
     }
 
 }
